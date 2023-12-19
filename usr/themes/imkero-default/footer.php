4aec6627ea3a1a2b1d80b14d7ae23fe38636cca0
电脑星人 <kerorek@outlook.com>
2023-12-19 11:03:51 +0000

    feat: 已登录用户不记录 pageview

--- usr/themes/imkero-default/footer.php
+++ usr/themes/imkero-default/footer.php
@@ -9,7 +9,9 @@
     <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动'); ?>.
 </footer><!-- end #footer -->
 
+<?php if (!$this->user->hasLogin()): ?>
 <script async src="<?php $this->options->themeUrl('pgview.js'); ?>" data-website-id="7eacac23-c4f2-4053-8435-fd989fec95ae" data-host-url="https://pageview.imkero.net"></script>
+<?php endif; ?>
 
 <?php $this->footer(); ?>
 </body>
