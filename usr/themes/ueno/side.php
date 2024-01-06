5a76e03b1f9f918530281d0c8bbbeb9bc1dc140e
电脑星人 <kerorek@outlook.com>
2024-01-06 09:51:12 +0000

    feat: 登出按钮增加 confirm 确认

--- usr/themes/ueno/side.php
+++ usr/themes/ueno/side.php
@@ -49,8 +49,16 @@
           </a>
         </li>
         <li class="side-nav-more">
-          <a href="<?php $this->options->logoutUrl(); ?>"><?php _e('登出'); ?></a>
+          <a href="javascript:void(0);" onclick="confirmLogout()"><?php _e('登出'); ?></a>
         </li>
+        <script>
+          function confirmLogout() {
+            var result = window.confirm("确定要登出吗？");
+            if (result) {
+              location.href = <?php echo json_encode($this->options->logoutUrl); ?>;
+            }
+          }
+        </script>
     <?php endif; ?>
     </ul>
     <hr class="side-hr">
