6ebb3071b1f8c6a4c9627c1a67ec1d3278cdcd59
imkero <kerorek@outlook.com>
2024-01-10 13:21:38 +0800

    revert: 误修改 Rows.php

--- var/Widget/Metas/Category/Rows.php
+++ var/Widget/Metas/Category/Rows.php
@@ -83,7 +83,7 @@ class Rows extends Metas
     {
         $parameter->setDefault('ignore=0&current=');
 
-        $select = $this->select();
+        $select = $this->select()->where('type = ?', 'category');
 
         $categories = $this->db->fetchAll($select->order('table.metas.order', Db::SORT_ASC));
         foreach ($categories as $category) {
