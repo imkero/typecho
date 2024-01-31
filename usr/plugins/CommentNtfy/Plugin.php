a21d2ee4c61154d8eb5bac8eddc732ac17efa718
imkero <kerorek@outlook.com>
2024-01-31 14:25:47 +0800

    feat: CommentNtfy 推送内容增加文章标题; 支持发送测试通知

--- usr/plugins/CommentNtfy/Plugin.php
+++ usr/plugins/CommentNtfy/Plugin.php
@@ -2,11 +2,13 @@
 namespace TypechoPlugin\CommentNtfy;
 
 use Widget\Options;
+use Widget\Notice;
 use Typecho\Http\Client;
 use Typecho\Plugin\PluginInterface;
 use Typecho\Widget\Helper\Form;
 use Typecho\Widget\Helper\Form\Element\Text;
 use Typecho\Widget\Helper\Form\Element\Radio;
+use Typecho\Widget\Helper\Form\Element\Submit;
 use Typecho\Response;
 use Utils\Helper;
 
@@ -25,23 +27,43 @@ class Plugin implements PluginInterface
     public static function activate()
     {
         \Typecho\Plugin::factory('Widget_Feedback')->finishComment = __CLASS__ . '::handleFinishComment';
-        \Typecho\Plugin::factory('Widget_Service')->commentNtfyAsync = __CLASS__ . '::commentNtfyAsync';//异步接口
+        \Typecho\Plugin::factory('Widget_Service')->commentNtfyAsync = __CLASS__ . '::commentNtfyAsync'; // 异步接口
     }
 
     public static function deactivate()
     {
     }
 
+    public static function getPluginOptions() {
+        return Options::alloc()->plugin('CommentNtfy');
+    }
+
     public static function config(Form $form)
     {
-      $ntfyServer = new Text('ntfyServer', null, 'https://ntfy.sh/', 'ntfy 服务器 URL');
-      $form->addInput($ntfyServer);
+        Options::alloc()->to($options);
 
-      $ntfyTopic = new Text('ntfyTopic', null, 'your-topic', 'ntfy topic');
-      $form->addInput($ntfyTopic);
+        $ntfyServer = new Text('ntfyServer', null, 'https://ntfy.sh/', 'ntfy 服务器 URL');
+        $form->addInput($ntfyServer);
 
-      $async = new Radio('async', array('0' => _t('不启用'), '1' => _t('启用'),), '0', '异步发送通知');
-      $form->addInput($async);
+        $ntfyTopic = new Text('ntfyTopic', null, 'your-topic', 'ntfy topic');
+        $form->addInput($ntfyTopic);
+
+        $async = new Radio('async', ['0' => _t('不启用'), '1' => _t('启用')], '0', '异步发送通知');
+        $form->addInput($async);
+
+        $testButton = new Submit('send-test-notification');
+        $testButton->value('发送测试通知');
+        $testButton->input->setAttribute('class', 'btn');
+        $testButton->input->setAttribute('id', 'test-button');
+        $testButton->input->setAttribute('value', '1');
+        $testButton->input->setAttribute('formaction', \Typecho\Common::url('/options-plugin.php?config=CommentNtfy&send-test-notification=1', $options->adminUrl));
+        $form->addItem($testButton);
+
+        if (isset($_GET['send-test-notification']) && $_GET['send-test-notification'] === '1') {
+            self::sendTestNotification();
+            Notice::alloc()->set('测试通知已发送', 'success');
+            Options::alloc()->response->goBack();
+        }
     }
 
     public static function personalConfig(Form $form)
@@ -50,13 +72,13 @@ class Plugin implements PluginInterface
 
     public static function handleFinishComment($comment)
     {
-        $pluginOptions = Options::alloc()->plugin('CommentNtfy');
+        $pluginOptions = self::getPluginOptions();
         if ($pluginOptions->async) {
             Helper::requestService('commentNtfyAsync', [
                 'coid' => $comment->coid,
             ]);
         } else {
-            self::sendNotification($comment);
+            self::sendCommentNotification($comment);
         }
     }
 
@@ -66,7 +88,7 @@ class Plugin implements PluginInterface
         $comment = Helper::widgetById('comments', $coid);
         if ($comment->have()) {
             self::fastEndResponse();
-            self::sendNotification($comment);
+            self::sendCommentNotification($comment);
         }
     }
 
@@ -85,10 +107,13 @@ class Plugin implements PluginInterface
         });
     }
 
-    public static function sendNotification($comment)
+    public static function sendCommentNotification($comment)
     {
         Options::alloc()->to($options);
-        $pluginOptions = Options::alloc()->plugin('CommentNtfy');
+        $pluginOptions = self::getPluginOptions();
+
+        $post = Helper::widgetById('contents', $comment->cid);
+        $postTitle = $post->title;
 
         $statusMap = [
             'approved' => '通过',
@@ -115,10 +140,32 @@ class Plugin implements PluginInterface
 
         $payload = [
             'topic' => $pluginOptions->ntfyTopic,
-            'message' => "评论者：{$comment->author} ({$comment->mail})\n评论内容：{$comment->text}\n状态：{$status}",
+            'message' => "文章: {$postTitle}\n评论者: {$comment->author} ({$comment->mail})\n评论内容: {$comment->text}\n状态: {$status}",
             'title' => $options->title . ' 有新的评论',
             'actions' => $actions,
         ];
+
+        self::sendNtfy($payload);
+    }
+
+    public static function sendTestNotification()
+    {
+        Options::alloc()->to($options);
+        $pluginOptions = self::getPluginOptions();
+
+        $payload = [
+            'topic' => $pluginOptions->ntfyTopic,
+            'message' => '如果您看到这条通知，说明您的 CommentNtfy 插件配置正确',
+            'title' => '测试通知',
+        ];
+
+        self::sendNtfy($payload);
+    }
+
+    private static function sendNtfy($payload)
+    {
+        Options::alloc()->to($options);
+        $pluginOptions = self::getPluginOptions();
 
         $client = Client::get();
         $client->setHeader('User-Agent', $options->generator)
